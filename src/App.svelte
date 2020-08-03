1636095237e90e0cf00cd0eeebfd4638ecad1fe4
annajliang <annaliangdev@gmail.com>
2020-08-03 17:14:39 -0400

    create modal component, import to app

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
 	import AnnouncementBar from './AnnouncementBar.svelte';
+	import Modal from './Modal.svelte';
 	import Header from './Header.svelte';
 	import Main from './Main.svelte';
 	import Footer from './Footer.svelte';
 </script>
 
 <AnnouncementBar />
+<Modal />
 <Header />
 <Main />
 <Footer />
