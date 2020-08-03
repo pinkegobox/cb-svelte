8f428d73f6b819000090e9a35c9424cebb5a4754
annajliang <annaliangdev@gmail.com>
2020-08-03 12:42:35 -0400

    finish navbar html

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -2,5 +2,14 @@
     import NavLinks from './NavLinks.svelte';
 </script>
 
-<NavLinks />
-<p>Navbar</p>
+<nav class="flexContainer">
+    <div>
+        <a href="/">
+        <img src="/assets/logo.png" class="logo" alt="CommerceBear Logo" />
+        </a>
+    </div>
+    <div class="flexContainer">
+        <NavLinks />
+        <a href="">Get Started</a>
+    </div>
+</nav>
