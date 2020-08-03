2dfacc248a6dfe2c037b9eaef1599b96e7e2ee41
annajliang <annaliangdev@gmail.com>
2020-08-03 15:32:17 -0400

    import navLinks to footer, add footer html

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,3 +1,30 @@
+<script>
+    import NavLinks from './NavLinks.svelte';
+</script>
+
 <footer>
-    Footer
+    <div className="wrapper">
+        <NavLinks />
+        <div class="contact">
+            <p>hello@commercebear.com</p>
+            <p>Canada +1 647 479 6243</p>
+            <p>USA +1 929 232 2327</p>
+        </div>
+        <div class="socialLinks">
+    
+        </div>
+        <p class="mailUpdates">
+            <a
+                href="https://mailchi.mp/commercebear/updates"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="This is an external link (opens in a new tab)"
+            >
+                Click here
+            </a>{" "}
+            to join our growing industry following and stay up to date with all
+            things CommerceBear.
+        </p>
+        <p>© 2020 CommerceBear. All Rights Reserved</p>
+    </div>
 </footer>
