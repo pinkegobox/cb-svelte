09fa2b77ccf5bd497cb749adc725243d25f9c5b1
annajliang <annaliangdev@gmail.com>
2020-08-03 17:23:22 -0400

    add modal html

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,1 +1,12 @@
-<p>Modal</p>
+<div class="container">
+    <h2>Don't miss a thing.</h2>
+    <p class="desc">
+        Sign up for our mailing list and get industry updates, live events, and
+        great analysis.
+    </p>
+    <form action="#" class="form">
+        <input type="text" placeholder="Email Address" />
+        <input type="button" value="Sign Up" />
+    </form>
+    <p class="disclaimer">We respect your privacy.</p>
+</div>
