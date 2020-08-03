50176c92560d24fb5f57dc83864ac5bbf83f9c81
annajliang <annaliangdev@gmail.com>
2020-08-03 14:47:12 -0400

    add styles for featureItem

--- src/FeatureItem.svelte
+++ src/FeatureItem.svelte
@@ -1,6 +1,14 @@
+<style>
+.desc {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+}
+</style>
+
 <li>
     <img src="/"/>
-    <div>
+    <div class="desc">
         <h3>test</h3>
         <p>test</p>
     </div>
