6443621b0d803cc6b48abf7a5373641e3347ff4f
annajliang <annaliangdev@gmail.com>
2020-08-03 17:01:07 -0400

    add announcementBar html and styles

--- src/AnnouncementBar.svelte
+++ src/AnnouncementBar.svelte
@@ -1,1 +1,21 @@
-<p>TEST</p>
+<style>
+    .container {
+    background-color: #545759;
+    color: #fff;
+    font-weight: bold;
+    font-size: 1.4rem;
+    text-align: center;
+    letter-spacing: 1px;
+    padding: 1.2rem 0;
+    position: relative;
+    z-index: 1;
+}
+</style>
+
+
+<div class="container">
+    <p>
+        <span role="img" aria-label="confetti emoji">🎉</span>See us in the press: Furniture Today, Business of Home, and Causal
+        Living
+    </p>
+</div>
