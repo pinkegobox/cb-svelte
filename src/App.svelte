9f1a4c81c8ac74cbb1dec52dd93121fbd06d7131
annajliang <annaliangdev@gmail.com>
2020-08-03 16:56:30 -0400

    create component for announcementBar, import to app

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
+	import AnnouncementBar from './AnnouncementBar.svelte';
 	import Header from './Header.svelte';
 	import Main from './Main.svelte';
 	import Footer from './Footer.svelte';
 </script>
 
+<AnnouncementBar />
 <Header />
 <Main />
 <Footer />
