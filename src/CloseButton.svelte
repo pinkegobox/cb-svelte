fca0b2c40adfed7d9e8a834669e9a554eef33aab
annajliang <annaliangdev@gmail.com>
2020-08-03 17:07:21 -0400

    add styles to closeButton

--- src/CloseButton.svelte
+++ src/CloseButton.svelte
@@ -1,3 +1,23 @@
+<style>
+.closeAnnoucement {
+    position: absolute;
+    top: 0;
+    right: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.closeAnnoucement:after {
+    content: '×';
+    display: block;
+    font-family: helvetica,arial,sans-serif;
+    font-weight: normal;
+    cursor: pointer;
+}
+</style>
+
+
 <span
     class="closeAnnoucement"
     tabIndex="0"
