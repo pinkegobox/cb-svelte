70ff8b25c4732c6a9ba4d3e027f1821e6dae3856
annajliang <annaliangdev@gmail.com>
2020-08-03 14:34:41 -0400

    finish main html

--- src/Main.svelte
+++ src/Main.svelte
@@ -3,5 +3,24 @@
     import Button from './Button.svelte';
 </script>
 
-<p>Main</p>
-<Features />
+<main class="wrapper">
+    <div class="desc">
+        <h2>
+            Generate new revenue, profits, and establish an effective
+            omni-channelbusiness.
+        </h2>
+        <h2>Over 53,720 furniture SKUs published.</h2>
+    </div>
+    <img src="/assets/ecommerce.png" alt="" />
+    <h2>
+        Designed to drive sales and automate operations.
+    </h2>
+    <Features />
+    <hr class="horizontalRule" />
+    <div class="learnMore">
+        <h2>
+            From start to scale, say hello to your next chapter of growth.
+        </h2>
+        <Button text="Talk to us to learn more" padding="2rem 3rem" url="https://calendly.com/sam-bear/30min?back=1&month=2020-08"/>
+    </div>
+</main>
